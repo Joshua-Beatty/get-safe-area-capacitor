f8dda94d3c9743135705c8c0f42a964692fd8b22
Joshua Beatty <joshuatb6@gmail.com>
2022-05-26 15:19:06 -0600

    Accounting for pixel ratio on android

--- example/src/index.js
+++ example/src/index.js
@@ -1,13 +1,14 @@
 import { StatusBar } from '@capacitor/status-bar';
 import { GetSafeArea } from 'get-safe-area-capacitor'
-
+import { Capacitor } from '@capacitor/core';
 (async () => {
+  if(Capacitor.getPlatform() == "android")
   await StatusBar.setOverlaysWebView({ overlay: true });
-  GetSafeArea.getSafeArea().then((e) => {
+  GetSafeArea().then((e) => {
      console.log("insets", JSON.stringify(e)) 
      var insets = document.createElement("div");
      insets.textContent = JSON.stringify(e) + " " + window.devicePixelRatio;
-     insets.style.marginTop = `${e.top / window.devicePixelRatio}px`; 
+     insets.style.marginTop = `${e.top}px`; 
      document.body.appendChild(insets);
     })
 })();
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-import { registerPlugin } from '@capacitor/core';
+import { Capacitor, registerPlugin } from '@capacitor/core';
 
 import type { GetSafeAreaPlugin } from './definitions';
 
-const GetSafeArea = registerPlugin<GetSafeAreaPlugin>('GetSafeArea', {
+const GetSafeAreaRegisteredPlugin = registerPlugin<GetSafeAreaPlugin>('GetSafeArea', {
   web: () => import('./web').then(m => new m.GetSafeAreaWeb()),
 });
+const GetSafeArea = async ()=>{
+  const insets = await GetSafeAreaRegisteredPlugin.getSafeArea()
+  
+  if(Capacitor.getPlatform() == "android"){
+    insets.top = insets.top / window.devicePixelRatio
+    insets.bottom = insets.bottom / window.devicePixelRatio
+    insets.right = insets.right / window.devicePixelRatio
+    insets.left = insets.left / window.devicePixelRatio
+  }
 
+  return insets
+}
 export * from './definitions';
 export { GetSafeArea };
